073a525cd2f87988676cf3a5c221ead4aeac0a43
thutasann <thutasann2002@gmail.com>
2025-03-05 11:34:32 +0800

    chore: bypass authorization request for now

--- src/nano-pulse-auth/src/main/java/com/thutasann/nano_pulse_auth/config/SecurityConfig.java
+++ src/nano-pulse-auth/src/main/java/com/thutasann/nano_pulse_auth/config/SecurityConfig.java
@@ -4,33 +4,31 @@ import java.util.Arrays;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.authentication.logout.LogoutHandler;
 import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
-import com.thutasann.nano_pulse_auth.filters.JwtAuthenticationFilter;
-
 import lombok.RequiredArgsConstructor;
 
+/**
+ * Security Configuration
+ * 
+ * @todo: Add Authorization Request
+ * @author Thuta San
+ * @version 1.0
+ * @since 2025-03-04
+ */
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
-
-        private final JwtAuthenticationFilter jwtAuthFilter;
-        private final AuthenticationProvider authenticationProvider;
 
         @Bean
         public LogoutHandler logoutHandler() {
@@ -40,33 +38,8 @@ public class SecurityConfig {
         @Bean
         public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
                 http
-                                .csrf(AbstractHttpConfigurer::disable)
-                                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                                 .authorizeHttpRequests(auth -> auth
-                                                .requestMatchers(
-                                                                "/api/v1/auth/**",
-                                                                "/v2/api-docs",
-                                                                "/v3/api-docs",
-                                                                "/v3/api-docs/**",
-                                                                "/swagger-resources",
-                                                                "/swagger-resources/**",
-                                                                "/configuration/ui",
-                                                                "/configuration/security",
-                                                                "/swagger-ui/**",
-                                                                "/webjars/**",
-                                                                "/swagger-ui.html")
-                                                .permitAll()
-                                                .anyRequest().authenticated())
-                                .sessionManagement(session -> session
-                                                .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                                .authenticationProvider(authenticationProvider)
-                                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
-                                .logout(logout -> logout.logoutUrl("/api/v1/auth/logout")
-                                                .addLogoutHandler(logoutHandler())
-                                                .logoutSuccessHandler(
-                                                                (request, response,
-                                                                                authentication) -> SecurityContextHolder
-                                                                                                .clearContext()));
+                                                .anyRequest().permitAll());
 
                 return http.build();
         }

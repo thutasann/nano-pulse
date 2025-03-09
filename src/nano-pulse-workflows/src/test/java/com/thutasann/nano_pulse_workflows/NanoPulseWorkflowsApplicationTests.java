f04f902c24582fbd1d890eabc62afabfc7b434d2
thutasann <thutasann2002@gmail.com>
2025-03-09 19:07:25 +0800

    chore(workflows): workflows service init

---
+++ src/nano-pulse-workflows/src/test/java/com/thutasann/nano_pulse_workflows/NanoPulseWorkflowsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thutasann.nano_pulse_workflows;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NanoPulseWorkflowsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
